e4770162e80286abc08464dafde4ac9968e69d40
HannaPadym <120955272+HannaPadym@users.noreply.github.com>
2023-02-05 12:05:21 +0300

    Add files via upload

---
+++ MouseStudy1.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class MouseStudy1 {
+    static JFrame jFrame;
+    static JLayeredPane jLayeredPane;
+
+    public static void main(String[] args) {
+        jFrame = getFrame();
+        jLayeredPane = new JLayeredPane();
+        jFrame.add(jLayeredPane);
+
+
+        jFrame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (e.getButton() == 1) {
+                    JLabel jLabel = new JLabel("X: " + e.getX() + ", Y: " + e.getY());
+                    jLabel.setBounds(e.getX(), e.getY(), 100, 20);
+                    jLayeredPane.add(jLabel);
+                }
+
+
+            }
+        });
+
+    }
+
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame("Task1");
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(0, 0, dimension.width, dimension.height);
+        return jFrame;
+    }
+}
